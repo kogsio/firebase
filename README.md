812a64311d5cd399b31a9639d7f1938a52201110
abelsan <abel@mit.edu>
2020-10-27 19:45:06 -0400

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # firebase
-Serverless services
+Code for Firebase serverless services - web app.
+
+## Compete before using code
+1. Go to firebase, https://firebase.google.com/
+1. Sign in
+1. Go to console
+1. Create new project
+1. Select "project settings" add web app
+1. Copy JS configuration, you will need it in your code
+1. Enable service (e.g. authentication)
+
+## Replace firebaseConfig in code with yours 
+``` 
+var firebaseConfig = {
+    apiKey: "AIzaSyADyt14nFurAg9XuZxVEU1azmcA3Ws6F_w",
+    authDomain: "mocko-f1ff6.firebaseapp.com",
+    databaseURL: "https://mocko-f1ff6.firebaseio.com",
+    projectId: "mocko-f1ff6",
+    storageBucket: "mocko-f1ff6.appspot.com",
+    messagingSenderId: "928941901381",
+    appId: "1:928941901381:web:00e166aa693ef7a9d4852c"
+};
+```
--- login.js
+++ login.js
@@ -3,14 +3,13 @@
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
-        apiKey: "AIzaSyBboZP3WoD5Os9mOv4QGWej9Cu5lmI-bIM",
-        authDomain: "course-92aae.firebaseapp.com",
-        databaseURL: "https://course-92aae.firebaseio.com",
-        projectId: "course-92aae",
-        storageBucket: "course-92aae.appspot.com",
-        messagingSenderId: "906967504618",
-        appId: "1:906967504618:web:a5b51551a229502dc7a993",
-        measurementId: "G-PWBBC8GN6B"
+		apiKey: "AIzaSyADyt14nFurAg9XuZxVEU1azmcA3Ws6F_w",
+		authDomain: "mocko-f1ff6.firebaseapp.com",
+		databaseURL: "https://mocko-f1ff6.firebaseio.com",
+		projectId: "mocko-f1ff6",
+		storageBucket: "mocko-f1ff6.appspot.com",
+		messagingSenderId: "928941901381",
+		appId: "1:928941901381:web:00e166aa693ef7a9d4852c"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
